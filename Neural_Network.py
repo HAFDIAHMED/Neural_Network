cf61be1827af09a01d676748130c5b5a3d965ed2
HAFDIAHMED <ahafdi62@gmail.com>
2020-11-22 13:54:11 +0100

    initial commit

---
+++ Neural_Network.py
@@ -0,0 +1,116 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+1+2
+
+
+# In[ ]:
+
+
+
+
+
+# In[2]:
+
+
+print(1)
+
+
+# In[3]:
+
+
+x="name"
+
+
+# In[4]:
+
+
+x<
+
+
+# In[5]:
+
+
+x
+
+
+# In[7]:
+
+
+def retouur( n) :
+     return n
+
+
+# In[10]:
+
+
+retoour( x)
+
+
+# In[11]:
+
+
+print(x)
+
+
+# In[12]:
+
+
+import numpy
+
+
+# In[14]:
+
+
+numpy.binary_repr(12)
+
+
+# In[15]:
+
+
+def NN(m1,m2,w1,w2,b):
+    return sigmoid(w1*m1+w2*m2+b)
+def sigmoid(z):
+    return 1/(1+numpy.exp(-z))
+
+
+# In[17]:
+
+
+w1=numpy.random.randn()
+w2=numpy.random.randn()
+b=numpy.random.randn()
+
+
+# In[18]:
+
+
+w1
+
+
+# In[19]:
+
+
+w2
+
+
+# In[20]:
+
+
+b
+
+
+# In[21]:
+
+
+NN(3,1.5,w1,w2,b)
+
+
+# In[ ]:
+
+
+
+
